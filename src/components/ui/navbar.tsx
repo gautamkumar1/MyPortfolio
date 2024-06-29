2123cfb74462e6a75c77eb445e20457e5db35512
gautamkumar1 <gautamraaz936@gmail.com>
2024-06-29 19:47:22 +0530

    update package.json, pnpm-lock.yaml and navbar.tsx

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -3,34 +3,38 @@ import { Sheet, SheetTrigger, SheetContent } from "@/components/ui/sheet";
 import { Button } from "@/components/ui/button";
 import { JSX, SVGProps } from "react";
 import { getData } from "@/lib/serverUtils";
+import { ModeToggle } from "../toogle-button-darkMode";
 
 
 export default async function Navbar() {
   const data = await getData();
 
   return (
-    <header className="fixed top-0 z-50 w-full bg-[#121212] shadow-sm flex h-16 items-center justify-between px-4 md:px-6">
+    <header className="fixed top-0 z-50 w-full shadow-sm flex h-16 items-center justify-between px-4 md:px-6 ">
       <div className="container max-w-5xl mx-auto flex h-18 items-center justify-between py-2 px-4 md:px-6">
         <Link href="#" className="flex items-center" prefetch={false}>
-          <div className='text-2xl text-white font-bold underline underline-offset-8 decoration-green-500 -rotate-6'>𝒢𝒶𝓊𝓉𝒶𝓂 👨🏻‍💻 </div>
+          <div className='text-2xl text-[#ce7e00] font-bold underline underline-offset-8 decoration-green-500 -rotate-6'>Gautam 👨🏻‍💻 </div>
         </Link>
-        <nav className="hidden space-x-4 lg:flex">
+        <nav className="hidden space-x-4 lg:flex items-center">
           {data.visual.navbar.links.map((item) => (
             <Link
               href={item.path}
               key={item.path}
-              className="text-sm transition-colors text-white-800 font-bold hover:text-primary dark:hover:text-[#23C55F] dark:text-white-800"
+              className="text-sm transition-colors text-[#8A8A8A] font-bold hover:text-primary dark:hover:text-[#23C55F] dark:text-white-800"
             >
               {item.label}
             </Link>
           ))}
-          {/* Resume Btn */}
-          <Link href="assets/Gautam Kumar Resume.pdf" download="Gautam Kumar Resume" target="_blank" className="text-sm transition-colors text-white-800 font-bold hover:text-primary dark:hover:text-[#23C55F]">
-            <p className="text-text-xs text-teal-400">𝑹𝒆𝒔𝒖𝒎𝒆 📜</p>
-          </Link>
-
-          
+          <div className="flex items-center space-x-4">
+            {/* Resume Btn */}
+            <Link href="assets/Gautam Kumar Resume.pdf" download="Gautam Kumar Resume" target="_blank" className="text-sm transition-colors text-[#23C55F] font-bold hover:text-primary dark:hover:text-[#23C55F]">
+              <p className="w-[150px] border-[2px] border-solid border-green-200 px-5 py-3 bg-dark-100 rounded-[30px] scale-[.90] hover:scale-[.95] transition-all">Resume 📜</p>
+            </Link>
+            {/* Mode Toggle Dark Mode */}
+            <ModeToggle />
+          </div>
         </nav>
+
         <Sheet>
           <SheetTrigger asChild>
             <Button variant="outline" size="icon" className="lg:hidden">
@@ -44,15 +48,19 @@ export default async function Navbar() {
                 <Link
                   href={item.path}
                   key={item.path}
-                  className="text-sm transition-colors text-white-800 font-bold hover:text-primary dark:hover:text-[#23C55F]"
+                  className="text-sm transition-colors text-[#8A8A8A] font-bold hover:text-primary dark:hover:text-[#23C55F]"
                 >
                   {item.label}
                 </Link>
               ))}
-              {/* Resume Btn */}
-              <Link href="assets/Gautam Kumar Resume.pdf" download="Gautam Kumar Resume" target="_blank" className="text-sm transition-colors text-white-800 font-bold hover:text-primary dark:hover:text-[#23C55F]">
-                <p className="text-text-xs text-teal-400">𝑹𝒆𝒔𝒖𝒎𝒆 📜</p>
-              </Link>
+              <div className="flex items-center space-x-4">
+            {/* Resume Btn */}
+            <Link href="assets/Gautam Kumar Resume.pdf" download="Gautam Kumar Resume" target="_blank" className="text-sm transition-colors text-[#23C55F] font-bold hover:text-primary dark:hover:text-[#23C55F]">
+              <p className="w-[150px] border-[2px] border-solid border-green-200 px-5 py-3 bg-dark-100 rounded-[30px] scale-[.90] hover:scale-[.95] transition-all">Resume 📜</p>
+            </Link>
+            {/* Mode Toggle Dark Mode */}
+            <ModeToggle />
+          </div>
             </div>
           </SheetContent>
         </Sheet>
